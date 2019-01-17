2824755628f2b82289b8f08c833e84b6a1de3c0c
m053m716 <max.murphy11@gmail.com>
2019-01-17 16:02:25 -0600

    Add parsing of string channel and numeric channel ID so that it is associated with each element of Channels property struct (in addition to 'probe' sub-field). -MM

--- +nigeLab/@Block/parseProbeNumbers.m
+++ +nigeLab/@Block/parseProbeNumbers.m
@@ -10,24 +10,43 @@ flag = false;
 switch blockObj.RecType
    case 'Intan'
       % Make assumptions that 2 unique board_stream ID in same port are
-      % from a dual-headstage plugged into 2 different electrode arrays.
+      % from a dual-headstage plugged into 2 different electrode arrays
+      % (since that is the typical use-case at Nudo Lab)
       portStream = nan(numel(blockObj.Channels),2);
       for iCh = 1:numel(blockObj.Channels)
          portStream(iCh,:) = [blockObj.Channels(iCh).port_number, ...
-                              blockObj.Channels(iCh).board_stream];
+            blockObj.Channels(iCh).board_stream];
       end
+      
       [~,~,probe] = unique(portStream,'rows');
       for iCh = 1:numel(blockObj.Channels)
          blockObj.Channels(iCh).probe = probe(iCh);
+         
+         [blockObj.Channels(iCh).chNum,...
+            blockObj.Channels(iCh).chStr] = getChannelNum(...
+            blockObj.Channels(iCh).custom_channel_name);
       end
+      
+      
    case 'TDT'
       for iCh = 1:numel(blockObj.Channels)
          blockObj.Channels(iCh).probe = blockObj.Channels(iCh).port_number;
+         [blockObj.Channels(iCh).chNum,...
+            blockObj.Channels(iCh).chStr] = getChannelNum(...
+            blockObj.Channels(iCh).custom_channel_name);
       end
+      
    otherwise
       warning('%s is not a supported RecType.',blockObj.RecType);
       return;
 end
 flag = true;
+
+   function [channelNum,channelString] = getChannelNum(channelName)
+      %% GETCHANNELNUM  Get numeric and string values for channel NUMBER
+      numericIndex = regexp(channelName, '\d');
+      channelString = channelName(numericIndex);
+      channelNum = str2double(channelString);
+   end
 
 end
