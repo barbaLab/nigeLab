3e8dfa950c42888afc07427d2eb10a8460d7d641
Federico <fedbarban+github@gmail.com>
2019-03-28 14:27:14 +0100

    Optimized filtering procedure
    defualts.Filt - you can now chose the filter type here. 
    Support for iir, sos and fir filters. Three different filter are already implemented: 4th order butter and elliptic, intan unipolar and a more complex SOS elliptic one. 
    doUnitFilter - a more optimized zerophase filtering procedure is implemented. Based on a compiled mex file, thers is still room for improvement in the c code. More or less 60% speedup and identical output as filtiflt. Filtering is done directly in single precision. This can be changed without much of loss in computational performance if needed in future.

--- +nigeLab/@Block/doUnitFilter.m
+++ +nigeLab/@Block/doUnitFilter.m
@@ -27,32 +27,38 @@ end
 fType = blockObj.FileType{strcmpi(blockObj.Fields,'Filt')};
 
 %% DESIGN FILTER
-bp_Filt = designfilt('bandpassiir', 'StopbandFrequency1', pars.FSTOP1, ...
-   'PassbandFrequency1', pars.FPASS1, ...
-   'PassbandFrequency2', pars.FPASS2, ...
-   'StopbandFrequency2', pars.FSTOP2, ...
-   'StopbandAttenuation1', pars.ASTOP1, ...
-   'PassbandRipple', pars.APASS, ...
-   'StopbandAttenuation2', pars.ASTOP2, ...
-   'SampleRate', blockObj.SampleRate, ...
-   'DesignMethod', pars.METHOD);
+[b,a,zi,nfact,L] = pars.getFilterCoeff(blockObj.SampleRate);
 
 %% DO FILTERING AND SAVE
 fprintf(1,'\nApplying bandpass filtering... ');
 fprintf(1,'%.3d%%',0)
 updateFlag = false(1,blockObj.NumChannels);
 for iCh = blockObj.Mask
+   if blockObj.Channels(iCh).Raw.length <= nfact      % input data too short
+      error(message('signal:filtfilt:InvalidDimensionsDataShortForFiltOrder',num2str(nfact)));
+   end
    if ~pars.STIM_SUPPRESS
       % Filter and and save amplifier_data by probe/channel
       pNum  = num2str(blockObj.Channels(iCh).port_number);
       chNum = blockObj.Channels(iCh).custom_channel_name(...
-         regexp(blockObj.Channels(iCh).custom_channel_name, '\d'));
-      data = single(filtfilt(bp_Filt,blockObj.Channels(iCh).Raw.double));
-      
+         regexp(blockObj.Channels(iCh).custom_channel_name, '\d'));      
       fName = sprintf(strrep(blockObj.Paths.Filt.file,'\','/'), ...
          pNum, chNum);
+      
+      % bank of filters. This is necessary when the designed filter is high
+      % order SOS. Otherwise L should be one. See the filter difinition
+      % params in defualt.Filt
+      data = blockObj.Channels(iCh).Raw(:);
+      for ii=1:L
+         data = (ff(b,a,data,nfact,zi));
+      end
+      
       blockObj.Channels(iCh).Filt = nigeLab.libs.DiskData(...
-         fType,fName,data,'access','w');
+         fType,fName,data,...
+         'access','w',...
+         'size',size(data),...
+         'class',class(data));
+      
       blockObj.Channels(iCh).Filt = lockData(blockObj.Channels(iCh).Filt);
    else
       warning('STIM SUPPRESSION method not yet available.');
@@ -71,3 +77,40 @@ flag = true;
 blockObj.save;
 end
 
+function Y = ff(b,a,X,nEdge,IC)
+
+%%
+% nedge dimension of the edge effect
+% IC initial condition. can usually be computed as
+% K       = eye(Order - 1);
+% K(:, 1) = a(2:Order);
+% K(1)    = K(1) + 1;
+% K(Order:Order:numel(K)) = -1.0;
+% IC      = K \ (b(2:Order) - a(2:Order) * b(1));
+% where order is the filter order as 
+% Order = max(nb, na);
+
+% User Interface: --------------------------------------------------------------
+% Do the work: =================================================================
+% Create initial conditions to treat offsets at beginning and end:
+if ~iscolumn(X),X=X(:);end
+% Use a reflection to extrapolate signal at beginning and end to reduce edge
+% effects (BSXFUN would be some micro-seconds faster, but it is not available in
+% Matlab 6.5):
+Xi   = 2 * X(1) - X(2:(nEdge + 1));
+
+Xf   = 2 * X(end) - X((end - nEdge):(end - 1));
+
+% Use the faster C-mex filter function: -------------------------
+% Filter initial reflected signal:
+[~, Zi] = nigeLab.utils.FilterX.FilterX(b, a, Xi, IC * Xi(end),true);   
+
+% Use the final conditions of the initial part for the actual signal:
+[Ys, Zs]  = nigeLab.utils.FilterX.FilterX(b, a, X,  Zi);              % "s"teady state
+Yf        = nigeLab.utils.FilterX.FilterX(b, a, Xf, Zs, true);              % "f"inal conditions
+
+% Filter signal again in reverse order:
+[~, Zf] = nigeLab.utils.FilterX.FilterX(b, a, Yf, IC * Yf(1));  
+Y         = nigeLab.utils.FilterX.FilterX(b, a, Ys, Zf, true);
+Y = Y';
+end
