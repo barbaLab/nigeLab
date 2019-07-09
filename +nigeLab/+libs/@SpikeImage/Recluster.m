19d36131530fbcc9ce60d15f6d30cf22c263fb46
Federico <fedbarban+github@gmail.com>
2019-07-09 15:36:49 +0200

    Clustering bugfixes
    Fixed some bugs afflicting AutoClustering and code slightly cleaned.

--- +nigeLab/+libs/@SpikeImage/Recluster.m
+++ +nigeLab/+libs/@SpikeImage/Recluster.m
@@ -20,30 +20,31 @@ fprintf(1,'Performing reclustering...');
          return;
     end
     
-    if par.permut == 'y'
-       if par.match == 'y'
-          naux = min(par.max_spk,size(inspk,1));
-          ipermut = randperm(length(inspk));
-          ipermut(naux+1:end) = [];
-       else
-          ipermut = randperm(length(inspk));
-       end
-       inspk_aux = inspk(ipermut,:);
-    else
-       if par.match == 'y'
-          naux = min(par.max_spk,size(inspk,1));
-          inspk_aux = inspk(1:naux,:);
-       else
-          inspk_aux = inspk;
-       end
-    end
+%% TODO, permuting spikes
+%     if par.permut == 'y'
+%        if par.match == 'y'
+%           naux = min(par.max_spk,size(inspk,1));
+%           ipermut = randperm(length(inspk));
+%           ipermut(naux+1:end) = [];
+%        else
+%           ipermut = randperm(length(inspk));
+%        end
+%        inspk_aux = inspk(ipermut,:);
+%     else
+%        if par.match == 'y'
+%           naux = min(par.max_spk,size(inspk,1));
+%           inspk_aux = inspk(1:naux,:);
+%        else
+%           inspk_aux = inspk;
+%        end
+%     end
     
-
+%% Perfomring clustering
 
 
     [classes,temp] = nigeLab.utils.SPC.DoSPC(par,inspk);
 
-
+%% Moving spikes
    uclass = unique(classes);
    if iscolumn(uclass),uclass=uclass';end
 for ii= uclass
@@ -55,55 +56,4 @@ for ii= uclass
 end
 fprintf(1,' done.\n');
 
-end
-
-function [Temp,classes] = SPCrun(par,inspk_aux)
-       %Interaction with SPC
-
-    current_temp = max(temp);
-    classes = zeros(1,size(clu,2)-2);
-%     for c =1: length(clust_num)
-%        aux = clu(temp(c),3:end) +1 == clust_num(c);
-%        classes(aux) = c;
-%     end
-% Same but vectorized
- classes = sum( (clu(temp,3:end) +1 == clust_num) .* (1:numel(clust_num))',1);
-
-    
-    if par.permut == 'n'
-       classes = [classes zeros(1,max(size(spikes,1)-size(clu,2)-2,0))];
-    end
-    
-    [Temp,classes]=checkclasses(temp,classes);
-    
-%     clustering_results = [];
-%     clustering_results(:,1) = repmat(current_temp,length(classes),1); % temperatures
-%     clustering_results(:,2) = classes'; % classes
-%     
-%     for i=1:max(classes)
-%        clustering_results(classes==i,3) = Temp(i);
-%        clustering_results(classes==i,4) = clust_num(i); % original classes
-%     end
-%     
-%     clustering_results(:,5) = repmat(par.min_clus,length(classes),1);
-%     
-
-    
-end
-
-function [Temp,classes]=checkclasses(temp,classes)
-
-    Temp = temp;
-    % Classes should be consecutive numbers
-    classes_names = nonzeros(sort(unique(classes)));
-%     if sum(classes_names) ~= classes_names(end)*(classes_names(end)+1)/2
-       for i= 1:length(classes_names)
-          c = classes_names(i);
-          if c~= i
-             classes(classes == c) = i;
-          end
-          Temp(i) = temp(i);
-       end
-%     end
-    
 end
