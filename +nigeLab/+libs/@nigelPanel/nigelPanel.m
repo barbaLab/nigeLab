642cd565cb40f822b87443a6f3e4a1ef006ad20c
m053m716 <max.murphy11@gmail.com>
2019-12-01 11:05:22 -0600

    Add a lot of documentation to nigeLab.libs.nigelPanel.

--- +nigeLab/+libs/@nigelPanel/nigelPanel.m
+++ +nigeLab/+libs/@nigelPanel/nigelPanel.m
@@ -1,14 +1,43 @@
 classdef nigelPanel < handle
-%% NIGELPANEL Helper object for nigeLab GUI.
-% It builds a panel with a titlebox and a subtitle. The inside panel can
-% also be made scrollable.
+% NIGELPANEL  Helper object for nigeLab GUI. Container for other graphics.
+%
+% By default, nigeLab.libs.nigelPanel builds a panel with a title box and
+% subtitle. Conveniently, the panel can be made "scrollable" through some
+% interfacing with "jscrollpane," which is made accessible through the
+% "attachScrollPanelTo" function contributed by Yair Altman to the Matlab
+% File Exchange.
+%
+%    obj = nigeLab.libs.nigelPanel(parent);
+%    obj = nigeLab.libs.nigelPanel(parent,'Name1',value1,...);
+%  
+%    inputs:
+%    parent  --  Container for nigelPanel object. Any class that is
+%                a valid parent for matlab.ui.container.Panel is
+%                acceptable here.
+%    varargin  --  'Name', value input argument pairs that
+%                   correspond mostly to properties of
+%                   matlab.ui.container.Panel. Valid 'Name' values
+%                   include:
+%                   --> 'TitleBarColor' (see: 
+%                        nigeLab.defaults.nigelColors)
+%                   --> 'PanelColor' (see: 
+%                        nigeLab.defaults.nigelColors)
+%                   --> 'TitleColor' (see: 
+%                        nigeLab.defaults.nigelColors)
+%                   --> 'Position'
+%                   --> 'String'
+%                   --> 'Substr' 
+%                   --> 'Tag' (default is 'nigelPanel')
+%                   --> 'Units' ('normalized' or 'pixels')
+%                   --> 'Scrollable' ('off' (default) or 'on')
+%
 % Typical use:
 % 
 % F = figure;
 % p = nigeLab.libs.nigelPanel(F,...
 %             'String','ThisIsATitle',...
 %             'Tag','MyFisrtNigelPanel',...
-%             'Units','normalized'
+%             'Units','normalized',...
 %             'Position',[0 0 1 1],...
 %             'Scrollable','off',...
 %             'PanelColor',nigeLab.defaults.nigelColors('surface'),...
@@ -19,42 +48,62 @@ classdef nigelPanel < handle
 %                 % a nigelpanel
    
    properties
-      Parent
-      Tag
+      Parent % Handle to Parent container. Must be a class that is a valid parent for matlab.ui.container.Panel
+      Tag    % Short identifier that can be used to get this object from an array of nigelPanels
       Visible = true;
       InnerPosition;
-      Position;
-      String
-      Substr
+      Position; % 4-element numeric vector: [x,y,width,height]
+      String % Char array for string. Currently unused ...?
+      Substr % Char array that is a sub-string. Currently unused ...?
       pCols
       tCols
    end
    
    properties(SetObservable)
-      Units
-      Children
+      Units     % 'Normalized' or 'Pixels'
+      Children  % Cell Array of nigeLab.libs.nigelPanel objects
    end
    
    properties (Access = private)
-      OutPanel;
+      OutPanel;   % Handle to the uipanel that is an "outer" container
       ChildName 
-      panel;
+      panel;      % Handle to the uipanel that is the nigelPanel basically
       axes
       titleBox
    end
    
    methods
+      % Class constructor for nigeLab.libs.nigelPanel object
       function obj = nigelPanel(parent,varargin)
-         %% Constructor. Creates a uiPanel obj with a nice title and titlebox.
-         % 
+         % NIGELPANEL  Class constructor for nigelPanel, a standardized
+         %             custom graphics container for nigeLab.
+         %
+         %  obj = nigelPanel(parent);
+         %  obj = nigelPanel(parent,'Name1',value1,...,'NameK',valueK);
+         %
+         %  inputs:
+         %  parent  --  Container for nigelPanel object. Any class that is
+         %              a valid parent for matlab.ui.container.Panel is
+         %              acceptable here.
+         %  varargin  --  'Name', value input argument pairs that
+         %                 correspond mostly to properties of
+         %                 matlab.ui.container.Panel. Valid 'Name' values
+         %                 include:
+         %                 --> 'TitleBarColor' (see: 
+         %                      nigeLab.defaults.nigelColors)
+         %                 --> 'PanelColor' (see: 
+         %                      nigeLab.defaults.nigelColors)
+         %                 --> 'TitleColor' (see: 
+         %                      nigeLab.defaults.nigelColors)
+         %                 --> 'Position'
+         %                 --> 'String'
+         %                 --> 'Substr' 
+         %                 --> 'Tag' (default is 'nigelPanel')
+         %                 --> 'Units' ('normalized' or 'pixels')
+         %                 --> 'Scrollable' ('off' (default) or 'on')
          
          addlistener(obj,'Units','PostSet',@obj.UnitsChanged);
-         
-         set(parent,'Units','pixels');
-         ParentH =parent.Position(4);
-         
-         
-         TitleH = 0.055;
+
          Pars.TitleBarColor = [67 129 193]./255;   % to be loaded from colorscheme in the future
          Pars.PanelColor = [218 219 219]./255;   % to be loaded from colorscheme in the future
          Pars.TitleColor = [255 186 73]./255;
@@ -71,26 +120,33 @@ classdef nigelPanel < handle
          obj.String = Pars.String;
          obj.Substr = Pars.Substr;
          obj.Tag = Pars.Tag;
-         %          set(parent,'Units','normalized');
          
          p = uipanel(parent,...
              'BackgroundColor', Pars.PanelColor,...
              'Units',Pars.Units,...
              'Position',Pars.Position,...
              'BorderType','none');
-         %         set(p,'Units','pixels');
-         %         set(p,'Units','normalized');
+          
          p.BackgroundColor = Pars.PanelColor;
 
+        % What is "a"? Looks like a "narrow vertical axes" that is on the
+        % left-side of the uipanel p, so maybe it is a scroll bar. Except
+        % TitleH is hard-coded to 0.055, so TitleH * ParentH would give a
+        % value that is only 5.5% of parent height. So it's a tiny box in
+        % the lower-left corner? I've changed it so it starts out as
+        % normalized and is less confusing. [thought process of MM for FB
+        % benefit]
+        %
+        % "a" is an axes that lets us produce "nice curves" on the title
+        % box at the top of each nigelPanel.
         a = axes(p,...
            'Color','none',...
-           'Units','pixels',...
-           'Position',[0 5 5 ParentH*TitleH]);
-        set(a,'Units','normalized');
-        a.Position([1 3])=[0 1];
-        a.Position(2)=1-a.Position(4);
+           'Units','normalized',... 
+           'Position',[0 0.945 1 0.055]);
+
         Apos = a.Position;
-        a.XAxis.Visible='off';a.YAxis.Visible='off';
+        a.XAxis.Visible='off';
+        a.YAxis.Visible='off';
         
         titleBox.r1 = rectangle(a,...
            'Position',[0 0.5 1 0.5],...
@@ -151,11 +207,31 @@ classdef nigelPanel < handle
         
       end
       
+      % Returns the class, which is just obj.Tag
       function cl = class(obj)
+         % CLASS  Returns the Tag property as formatted char array
+         %
+         %  cl = obj.class;  
+         %
+         %  cl: Char array formatted as 'nigelPanel (obj.Tag)'
+         
          cl = sprintf('nigelPanel (%s)', obj.Tag);
       end
       
+      % Method to "nest" child objects into this panel
       function nestObj(this,Obj,name)
+         % NESTOBJ  Sets this nigelPanel as the parent of Obj and adds Obj
+         %           to cell array of nigelPanel.Children, along with an
+         %           associated name char array into cell array property
+         %           nigelPanel.ChildName.
+         %
+         %  this = nigeLab.libs.nigelPanel;
+         %  this.nestObj(graphicsObj,nameOfGraphicsObj);
+         %
+         %  e.g.
+         %  ax = axes();
+         %  this.nestObj(ax,'Information Axes');
+         
          if nargin < 3
             name = '';
          end
@@ -166,8 +242,22 @@ classdef nigelPanel < handle
       
       % Return handle to child object corresponding to 'name'
       function c = getChild(obj,name)
+         % GETCHILD  Return handle to child object corresponding to 'name'
+         %
+         %  c = obj.getChild('ChildName');
+         %
+         %  c  --  element of obj.Children that corresponds to the matching
+         %         element of obj.ChildName. If no matching element is
+         %         found, then a warning is issued and c is returned as an
+         %         empty array.
+         %
+         %  See Also:
+         %  nigeLab.libs.nigelPanel/NESTOBJ
+         
          idx = ismember(obj.ChildName,name);
          if isempty(idx)
+            warning('No member of %s property ChildName: %s',...
+               obj.class,name);
             c = [];
          else
             c = obj.Children{idx};
@@ -176,6 +266,19 @@ classdef nigelPanel < handle
       
       % Remove the child object corresponding to 'name'
       function removeChild(obj,name)
+         % REMOVECHILD  Remove the child object corresponding to
+         %              name ('ChildName').
+         %
+         %  obj.removeChild('ChildName'); 
+         %  
+         %  If it is a valid element of Children (determined by matching
+         %  ChildName to ChildName property), then remove it from both
+         %  Children and ChildName property arrays and delete the object
+         %  corresponding to Children.
+         %
+         %  See Also:
+         %  nigeLab.libs.nigelPanel/NESTOBJ
+         
          idx = ismember(obj.ChildName,name);
          if ~isempty(idx)
             if isvalid(obj.Children{idx})
@@ -186,31 +289,47 @@ classdef nigelPanel < handle
          end
       end
       
+      % Returns the pixel position for "panel"
       function pos = getPixelPosition(obj)
-          pos = getpixelposition(obj.panel);
+         % GETPIXELPOSITION  Returns the pixel position of "panel" object
+         
+         pos = getpixelposition(obj.panel);
       end
       
-      function UnitsChanged(obj, src, Event)
+      % Callback executed when Units property is changed
+      function UnitsChanged(obj, ~, Event)
+         % UNITSCHANGED  Callback that is executed when Units property is
+         %               changed. Makes sure that the panels will behave
+         %               properly if the Units property is toggled even
+         %               after the Constructor.
+         
          switch Event.AffectedObject.Units
             case 'normalized'
-                set(obj.OutPanel,'Units','normalized');
+               set(obj.OutPanel,'Units','normalized');
                obj.InnerPosition = [0 0 1 1];
                obj.Position = obj.OutPanel.Position;
             case 'pixels'
                set(obj.OutPanel,'Units','pixels');
                obj.InnerPosition = getpixelposition(obj.panel);
                obj.Position = obj.OutPanel.Position;
-
          end
-         
-
       end
       
-      function resizeInnerPanel(obj, src, Event)
-         % Doesn't need to do anything if all children are removed, maybe?
+      % Callback that is executed whenever a Child object is added or
+      % removed to ensure that the inner panel is the correct size.
+      function resizeInnerPanel(obj,~,~)
+         % RESIZEINNERPANEL  Callback executed whenever Children are added
+         %                   or removed from the Children cell array. It
+         %                   makes sure that the inner panel is the correct
+         %                   size.
+         
+         % This should be fixed to revert to some "initial" defaults that
+         % is agnostic to whether there are Children objects
+         
          if isempty(obj.Children)
             return;
          end
+         
          if obj.Children{end}.Position(2)<0
             panPos = getpixelposition(obj.panel);
             chPos = getpixelposition(obj.Children{end});
@@ -231,7 +350,8 @@ classdef nigelPanel < handle
    end
    
    methods (Access=private)
-       [hScrollPanel, hPanel] = attachScrollPanelTo(~,hObject)
+      % Method for accessing Java-based Scrollbar, provided by Yair Altman
+      [hScrollPanel, hPanel] = attachScrollPanelTo(~,hObject)
    end
 end
 
