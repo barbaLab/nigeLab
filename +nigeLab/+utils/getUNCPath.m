24933cde3b6c66c4be931c831a6bf853f3d57937
m053m716 <max.murphy11@gmail.com>
2019-12-04 14:27:40 -0600

    Made some changes to try and improve compatibility with UNIX.

--- +nigeLab/+utils/getUNCPath.m
+++ +nigeLab/+utils/getUNCPath.m
@@ -1,24 +1,68 @@
-function [ uncPath ] = getUNCPath( p )
-%% [ uncPath ] = getUNCPath( path )
-% returns UNC formatted path of the input path.
-if isempty(p),uncPath=p;return;end
+function UNCPath = getUNCPath(p)
+% GETUNCPATH  Returns UNC-formatted path of the input path
+%
+% UNCPath = getUNCPath(p)
+%
+%  p  --  Char array of a mapped path of interest (e.g. 'C:\...');
+%  UNCPath  --  Char array corresponding to "p" but with the universal
+%                 naming convention (UNC) applied.
+%
+%  Makes use of 'GetFullPath' package provided by Jan Simon via the Matlab
+%  File Exchange:
+%
+%  Jan (2019) 
+%  GetFullPath 
+%  (https://www.mathworks.com/matlabcentral/fileexchange/28249-getfullpath)
+%  MATLAB Central File Exchange. Retrieved December 4, 2019. 
 
-p = nigeLab.utils.GetFullPath.GetFullPath(p);
-pathParts = strsplit(p,filesep);
-if isempty(pathParts{1}),uncPath=p;return;end
-switch isunix
-    case true  % TODO. linux or mac.
-        
-       fprintf('TODO.')
-    case false
-        attempt = getUncPathFromMappedDrive(pathParts{1});
-        if isempty(attempt)
-            attempt = fullfile('\\localhost',lower(sprintf('%s$',pathParts{1}(1))));
-        end
-        pathParts{1} = attempt;
+%% Check inputs
+if isempty(p)
+   UNCPath=p;
+   return;
 end
 
-uncPath = fullfile(pathParts{:});
+% Handle cell inputs
+if iscell(p)
+   UNCPath = cell(size(p));
+   for i = 1:numel(p)
+      UNCPath{i} = getUNCPath(p{i});
+   end
+   return;
+end
+
+%% Return the Full path using FEX code
+p = nigeLab.utils.GetFullPath.GetFullPath(p);
+p = strrep(p,'\',filesep);
+pathParts = strsplit(p,filesep);
+if isempty(pathParts{1})
+   % This means it is already in UNC
+   UNCPath = p;      
+   return;
+end
+
+try
+   rootPath = getUncPathFromMappedDrive(pathParts{1});
+catch
+   rootPath = [];
+end
+
+if isempty(rootPath)
+   % Output depends on whether system is unix
+   if isunix % linux or mac
+      if (exist(p,'dir')==0) && (exist(p,'file')==0)
+         rootPath = ['~/' pathParts{1}];
+      else
+         rootPath = ['/' pathParts{1}];
+      end
+   else % windows
+      driveLetter = lower(pathParts{1}(1));
+      rootPath = ['\\localhost\' driveLetter '$'];
+   end
+end
+
+% Concatenate everything back together
+pathParts{1} = rootPath;
+UNCPath = fullfile(pathParts{:});
 
 end
 
