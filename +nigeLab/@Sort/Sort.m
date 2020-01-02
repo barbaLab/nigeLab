2a4e9f026cda1ee10a557f653f406513f725be7f
Max_KUMC_Desktop <max.murphy11@gmail.com>
2020-01-02 10:29:11 -0600

    Remove deprecated Block methods, update README

--- +nigeLab/@Sort/Sort.m
+++ +nigeLab/@Sort/Sort.m
@@ -20,30 +20,6 @@ classdef Sort < handle
 %   Provides a graphical interface to combine and/or restrict spikes
 %   included in each cluster. Works with spikes that have been extracted
 %   using the NIGELAB workflow.
-%
-% Originally by: Max Murphy
-% Modified by: Max Murphy & Fred Barban
-%
-%                   v3.0    01/07/2019  Integrate from "CRC" to orgExp.Sort
-%                   v2.1    10/03/2017  Added ability to handle multiple
-%                                       probes that have redundant input
-%                                       channel names.
-%                   v2.0    08/21/2017  Plots are done differently now, so
-%                                       that all spikes are plotted using
-%                                       imagesc instead of a random subset
-%                                       using plot. Imagesc is much faster
-%                                       and lets you see everything better.
-%                                       Working on adding "cutting" tool
-%                                       using imline to select subsets of
-%                                       spikes for new clusters.
-%                   v1.3    08/20/2017  Tried to switch code to
-%                                       object-oriented design with class
-%                                       definitions, etc.
-%                   v1.2    08/09/2017  Fixed bug with cluster radius norm.
-%                   v1.1    08/08/2017  Changed features display to have a
-%                                       drop-down menu that allows you to
-%                                       select which features to look at.
-%                   v1.0    08/04/2017  Original version (R2017a)
    
    %% PROPERTIES
    properties (SetAccess = public, GetAccess = public)
--- README.md
+++ README.md
@@ -1,22 +1,8 @@
-**UNDER DEVELOPMENT**
+# <img src="https://github.com/m053m716/ePhys_packages/tree/master/%2BnigeLab/img/nigeLab_Logo.PNG" style="zoom:10%;" /> nigeLab #
+> *Analysis tools by engineers, for physiologists.*
 
-# ePhys_packages #
-Packages for electrophysiology processing and analysis.
 
-## Table of Contents ##
-|    Package    |     Symbol    |
-|:-------------:|:-------------:|
-| [nigeLab](https://github.com/m053m716/ePhys_packages#-organize-experiments "nigeLab") | ![][nigeLab_Logo] |
 
----
-
-### ![][nigeLab_Logo] nigeLab ###
-Keep experiments organized with the [**nigeLab**](https://github.com/m053m716/ePhys_packages/tree/master/%2BnigeLab) package.  
-![nigeLab_Pipeline]
+Keep experiments organized with the [**nigeLab**](https://github.com/m053m716/ePhys_packages/wiki) package.  
+<img src="https://github.com/m053m716/ePhys_packages/tree/master/%2BnigeLab/img/DataPipeline_Overview.JPG" style="zoom:75%;" />
 _**Figure 1:** Moving from performing experiments to data endpoints is easy with nigeLab. Thanks to an intuitive user interface, no need to compile anything (everything runs in Matlab), and built-in flexibility that can be easily integrated to your workflow, nigeLab is a good choice to move from acquisition to analysis seamlessly._  
-  
----
-
-[nigeLab_Pipeline]: https://github.com/m053m716/ePhys_packages/blob/master/%2BnigeLab/img/DataPipeline_Overview.JPG "nigeLab Pipeline Overview"
-[nigeLab_LogoSmall]: https://github.com/m053m716/ePhys_packages/blob/master/%2BnigeLab/img/nigeLab_LogoSmall.PNG "nigeLab"
-[nigeLab_LogoLarge]: https://github.com/m053m716/ePhys_packages/blob/master/%2BnigeLab/img/nigeLab_Logo.PNG "nigeLab"
