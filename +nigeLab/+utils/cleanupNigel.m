ba4f17c2bd562f56c0ed9dc4b74a129c3d38a6dc
Federico <fedbarban+github@gmail.com>
2020-04-06 14:31:13 +0200

    Corrected cleanupNigel
    A couple of * were missing from the nigelPaths.

--- +nigeLab/+utils/cleanupNigel.m
+++ +nigeLab/+utils/cleanupNigel.m
@@ -1,11 +1,11 @@
 function cleanupNigel(tankPath)
-%CLEANUPNIGEL Deletes all nigelFiles at TOP-LEVEL of `tankPath` (input)
+%CLEANUPNIGEL Deletes all nigelFiles at the specified `tankPath` (input)
 %
 %   nigeLab.utils.cleanupNigel(tankPath);
 %   ```
 %       nigeLab.utils.cleanupNigel('/path/to/TANK');
 %   ```
-%       * The above would clear the following filetypes in /path/to/TANK:
+%       * The above would clear the following filetypes in /path/to/TANK and subfolders:
 %           + *_Pars.mat
 %           + *_Block.mat
 %           + *_Animals.mat
@@ -14,32 +14,35 @@ function cleanupNigel(tankPath)
 %           + *.nigelAnimal
 %           + *.nigelTank
 %
+%       This script will NOT delete data files such as channels files,
+%       Stream and Events
+%
 % -- Input --
 %   tankPath    :       (char array) Path of TANK folder to clean
 
-d = dir(fullfile(tankPath));
-[tankPath,tankName] = fileparts(tankPath);
-tankName = strrep(tankName,'_Tank','');
+d = dir(fullfile(fileparts(tankPath),'**','*_Tank.mat'));
+if isempty(d)
+    d = dir(fullfile(fileparts(tankPath),'*_Tank.mat'));
+end
+
+tankPath = d.folder;
+tankName = strrep(d.name,'_Tank.mat','');
 d = append(d,...
-    fullfile(tankPath,[tankName '_Pars.mat']));
+    dir(fullfile(tankPath,'**', '*_Pars.mat')));
 
 d = append(d,...
-    dir(fullfile(tankPath,'.nigelTank')));
+    dir(fullfile(tankPath,'**','.nigelTank')));
 d = append(d,...
-    dir(fullfile(tankPath,'_Pars.mat')));
-d = append(d,...
-    dir(fullfile(tankPath,'_Animal.mat')));
+    dir(fullfile(tankPath,'**','*_Animal.mat')));
 
 d = append(d,...
-    dir(fullfile(tankPath,'*','.nigelAnimal')));
+    dir(fullfile(tankPath,'**','.nigelAnimal')));
 d = append(d,...
-    dir(fullfile(tankPath,'*','_Block.mat')));
-d = append(d,...
-    dir(fullfile(tankPath,'*','_Pars.mat')));
+    dir(fullfile(tankPath,'**','*_Block.mat')));
 
 
 d = append(d,...
-    dir(fullfile(tankPath,'*','*','.nigelBlock')));
+    dir(fullfile(tankPath,'**','.nigelBlock')));
 
 % Iterates on elements of struct array `d`
 for ii=d'
@@ -48,5 +51,5 @@ end
 
 function c = append(a,b)
     a = reshape(a,1,numel(a));
-    b = reshape(a,1,numel(b));
+    b = reshape(b,1,numel(b));
     c = [a,b];
