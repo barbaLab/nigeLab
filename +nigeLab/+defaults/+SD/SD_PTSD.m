2c365986ff4af39c9b4aae41b23036c2e1e2a8f5
Marta <marta.care@iit.it>
2021-01-25 12:24:18 +0100

    Update PTSD default Method

--- +nigeLab/+defaults/+SD/SD_PTSD.m
+++ +nigeLab/+defaults/+SD/SD_PTSD.m
@@ -1,7 +1,7 @@
 function pars = SD_PTSD()
 %% function defining defualt parameters for SNEO spike detection algorithm
 
-pars.MultCoeff  = 5;  % Multiplication coefficient for noise
+pars.MultCoeff  = 4;  % Multiplication coefficient for noise
 % pars.Thresh     = 50;
 pars.RefrTime   = 0.5;  % [ms] Refractory time. 
 pars.PeakDur    =  2;   % [ms] Peak duration or pulse lifetime period
