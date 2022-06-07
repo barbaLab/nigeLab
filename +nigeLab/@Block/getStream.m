e381dd7d0a3c9d4fbceef8b4cc3572ae87fbc0e0
Federico Barban <fedbarban@gmail.com>
2022-06-07 09:36:26 +0200

    Struct2array
    Added struct2array back to getStream since it might be removed in mainstream matlab

--- +nigeLab/@Block/getStream.m
+++ +nigeLab/@Block/getStream.m
@@ -66,3 +66,18 @@ stream = struct2array(matchingNames);
 % stream = nigeLab.libs.nigelStream(blockObj,matchingNames{1},scaleOpts);
 
 end
+
+function a = struct2array(s)
+%STRUCT2ARRAY Convert structure with doubles to an array.
+
+%   Author(s): R. Losada
+%   Copyright 1988-2013 The MathWorks, Inc.
+
+narginchk(1,1);
+
+% Convert structure to cell
+c = struct2cell(s);
+
+% Construct an array
+a = [c{:}];
+end
