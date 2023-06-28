2856a294ea3f7c88250cd7419a442572e7306688
Federico Barban <fedbarban@gmail.com>
2023-06-28 17:20:25 +0200

    fixed multianimals issues
    - +defualts, added MultiAnimalsChar both at tank and animal
    - splitmultianimalsUI, it now copies time in the correct directory with the correct name
    - Animal, removed the MultiAnimals flag
        Animal/init removed assignment to multiAnimal flag
        splitMultiAnimals, removed assignment to multiAnimal flag
    - Block, removed the MultiAnimals flag, removed Time and Stim as a standalone field (now included in Meta), added linkMetaField method, removed some deprecated naming schemes
       getStatus, rationalized code
       init, removed assignment to multiAnimals flag
       intan2Block, now correctly saves Time ans Stim as Meta fields
       linkFields, now calls linkMetaFields when needed
       linkMetaFields, added. looks for Meta declared files and adds them to the Meta structure
       splitMultiAnimals, removed assignment to multiAnimals flag; Added a section to treat Meta.Stim data if present. It selects the stimulation channels present in the channels assigned to the current animal and creates a stim file with only those channels. It also changes the channel ID according to the new numeration system.
    - nigelObj, added MultiAnimals flag as a logical dependent property. If it finds the MultiAnimalsChar anywhere in the Meta struct (except for some exempted fields) it returns true.
        loadObj, added updateParams('KeepParams') on load.
       save, removed the section dealing with multianimals since it wasn't needed anymore.
       updatePaths, added a proper linkToData

--- +nigeLab/+defaults/Block.m
+++ +nigeLab/+defaults/Block.m
@@ -165,7 +165,7 @@ FieldType = { ...
    'Channels'; % 9
    'Streams';  % 10
    'Streams';  % 11
-   'Meta';  % 12
+   'Streams';  % 12
    'Meta';     % 13
    'Meta';     % 14
    'Meta';     % 15
