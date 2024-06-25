2d784d39509be82b9664108c04dd311e24f579d2
TommasoLambresa <tommylambresa@gmail.com>
2024-06-25 17:23:16 +0200

    Added adaptive version of SWTTEO
    Adaptive version of SWATTEO: it computes the threshold locally using a moving window that computes the percentile with the function movquant1(). peekseek modified in order to substitute the findpeaks matlab function.

---
+++ +nigeLab/+defaults/+SD/SD_AdSWTTEO.m
@@ -0,0 +1,17 @@
+function pars = SD_AdSWTTEO()
+%% function defining defualt parameters for SWTTEO spike detection algorithm
+pars.wavLevel   = 2;                % Wavelet decomposition level
+pars.waveName    = 'sym6';           % wavelet type
+
+pars.winType    = @hamming;        % function handle for the smoothing window type; This is fed to window function
+pars.smoothN    = 25;              % Number of samples for the smoothing operator. Set to 0 to turn off smoothing
+pars.winPars    = {'symmetric'};    % Optional parameters for the smoothing window
+
+pars.RefrTime   = 1;                 % [ms] refractory time
+pars.MultCoeff  = 7;               % Moltiplication coefficient for SWTTEO thresholding
+pars.Polarity   = -1;
+pars.PeakDur    =  2;   % [ms] Peak duration or pulse lifetime period
+pars.medWdw     = 0.01;   % [ms] Length of the window to compute the moving percentile
+pars.step       = 5;       % [#samples] to skip to center the window
+
+end
