cb2ca112f14326a8c5838db26a0c990b4bcebae6
TommasoLambresa <tommylambresa@gmail.com>
2025-01-23 18:30:19 +0100

    Introduced overshoot parametrization
    Now the lenght of the overshoot wdw for boundary problem can be set in the pars struct.

--- +nigeLab/+defaults/+SD/SD_AdSWTTEO.m
+++ +nigeLab/+defaults/+SD/SD_AdSWTTEO.m
@@ -1,17 +1,17 @@
 function pars = SD_AdSWTTEO()
-%% function defining defualt parameters for SWTTEO spike detection algorithm
+%% function defining defualt parameters for AdSWTTEO spike detection algorithm
 pars.wavLevel   = 2;                % Wavelet decomposition level
-pars.waveName    = 'sym6';           % wavelet type
+pars.waveName    = 'sym5';           % wavelet type
 
 pars.winType    = @hamming;        % function handle for the smoothing window type; This is fed to window function
 pars.smoothN    = 1;              % [ms] for the smoothing operator. Set to 0 to turn off smoothing
 pars.winPars    = {'symmetric'};    % Optional parameters for the smoothing window
 
-pars.RefrTime   = 1;                 % [ms] refractory time
+pars.RefrTime   = 0.5;                 % [ms] refractory time
 pars.MultCoeff  = 6;               % Moltiplication coefficient for SWTTEO thresholding
 pars.Polarity   = -1;
 pars.PeakDur    =  2.3;   % [ms] Max peak duration or pulse lifetime period
 pars.medWdw     = 0.003;   % [s] Length of the window to compute the moving quantile
-% pars.step       = 500;       % [#samples] to skip to center the window
 pars.k          = 3;       %number of samples for the TEO
+pars.overshoot  = 0.0007;  % [s] length of the secondary window for the boundary control
 end
--- +nigeLab/@Block/private/SD_AdSWTTEO.m
+++ +nigeLab/@Block/private/SD_AdSWTTEO.m
@@ -1,45 +1,60 @@
 function [ts,p2pamp,pmin,pW,E] = SD_AdSWTTEO(data,pars)
+%
 % New adaptive threshold SWTTEO, same procedure as normal SWTTEO but the
 % threshold on the energy is computed locally.
 %
-%SWTTEO Detects Spikes Location using a modified WTEO approach
-%   Usage:  spikepos = swtteo(in);
-%           spikepos = swtteo(in,params);
+%   Usage:
+%       [ts,~,~,~,~] = AdSWTTEO(data);  to obtain only spikes using default
+%                                       parameters
+%       pars = pars_AdSWTTEO;
+%       [ts,~,~,~,~] = AdSWTTEO(data, pars); to set custom parameters.
 %
 %   Input parameters:
-%       in_struc:   Input structure which contains
-%                       M:      Matrix with data, stored columnwise
-%                       SaRa:   Sampling frequency
-%       optional input parameters:
-%                       none
+%       data:   Input single-channel signal 1xN
+%       pars:   Configurable parameters for the detection
+%                   (look at pars_AdSWTTEO for description)
 %   Output parameters:
-%       spikepos:   Timestamps of the detected spikes stored columnwise
+%       ts:     Timestamps of the detected spikes stored in 1xNspks vector.
+%       p2pamp: peak-to-peak amplitude
+%       pmin:   amplitude in volt at spike's positions
+%       p2pamp: peak-to-peak amplitude
+%       p2pamp: peak-to-peak amplitude
 %
 %   Description:
-%       swtteo(in,params) computes the location of action potential in
-%       noisy MEA measurements. This method is based on the work of N.
-%       Nabar and K. Rajgopal "A Wavelet based Teager Engergy Operator for
-%       Spike Detection in Microelectrode Array Recordings". The algorithm
-%       therein was further improved by using a stationary wavelet
-%       transform and a different thresholding concept.
-%       For an unsupervised usage the sensitivity of the algorithm can be
-%       adapted by changing the value of the variable global_fac in line
-%       108. A larger value results in fewer detected spikes but also the
-%       number of false positives decrease. Decreasing this factor makes it
-%       more sensitive to detect spikes.
+%       AdSWTTEO(data,pars) computes the location of action potential in noisy
+%       MEA measurements. This method is based on the work of Lieb et al. 2017
+%       "A Stationary Wavelet Transform and a Time-Frequency Based Spike
+%       Detection Algorithm for Extracellular Recorded Data". The algorithm
+%       therein was further improved by using a windowed quantile and a peak
+%       refinement strategy. Modify 'MultCoeff' parameter to increase/decrease the
+%       threshold. A larger value results in fewer detected spikes but also the
+%       number of false positives decrease. Decreasing this factor makes it more
+%       sensitive to detect spikes.
 %
 %   References:
 %       tbd.
 %
 %
-%   Author: F. Lieb, February 2016
-%   Modified by: Tommaso Lambresa 19/06/2024 :)
+%   Author: Tommaso Lambresa 19/06/2024
 
 
 %parse inputs
+if nargin < 2
+    pars = pars_AdSWTTEO;
+    pars.fs = 25000; % to adapt to your sampling frequency
+    warning("Sampling frequency is not specified, using default 25 kHz")
+end
+
+if numel(fieldnames(pars)) ~= 13
+    error("pars structure lacks of some field.")
+end
+
 fs = pars.fs;
 pars.smoothN = round(pars.smoothN*1e-3*fs);
+
+% Defining TEO operator
 TEO = @(x,k) (x.^2 - myTEOcircshift(x,[-k, 0]).*myTEOcircshift(x,[k, 0]));
+
 L = length(data);
 data = data(:);     % ensure in is column
 
@@ -92,15 +107,14 @@ clear('ss');
 
 % Standard detection
 
-% lambda_swtteo   = nigeLab.utils.movquant1(out_,.99,pars.medWdw*fs,pars.step,1);
-lambda_swtteo   = nigeLab.utils.mymovquant(out_,.99,pars.medWdw*fs);
+lambda_swtteo   = mymovquant(out_,.99,pars.medWdw*fs);
 lambda_data      =  pars.MultCoeff*median(abs(data));
 data_th = zeros(size(data));
 data_th(out_>lambda_swtteo) = pars.Polarity .* data(out_>lambda_swtteo);
 
 minTime = 1e-3*pars.RefrTime; % parameter in milliseconds
-% [ts,pmin] = nigeLab.utils.peakseek(data_th,minTime*pars.fs,lambda_data);
-[ts_tmp,~] = nigeLab.utils.peakseek(abs(data_th),minTime*pars.fs);
+% [ts,pmin] = mypeakseek(data_th,minTime*pars.fs,lambda_data);
+[ts_tmp,~] = mypeakseek(abs(data_th),minTime*pars.fs);
 % pmin = pmin .* pars.Polarity;
 
 
@@ -119,7 +133,7 @@ for i = 1:length(ts_tmp)
     % Check the boundaries of the main window
     if locs == 1 || locs == length(signal_window)
         % Define secondary, smaller interval
-        overshoot = 0.0005;
+        overshoot = pars.overshoot;
         secondary_size = round(overshoot * pars.fs); % Half the size of the main window
         if locs == 1
             secondary_interval = max(1, interval(1) - secondary_size):interval(1);
@@ -214,13 +228,13 @@ if h > 1
     tlocmax(tlocmax < 1) = 1;
     tlocmax(tlocmax > numel(data)) = numel(data);
     for ii=1:size(tlocmin,2)
-        [thispeak, ~] = nigeLab.utils.peakseek(data(tlocmin(:,ii)),[],[],1);
+        [thispeak, ~] = mypeakseek(data(tlocmin(:,ii)),[],[],1);
         if isempty(thispeak)
             thispeak = PLP+1;
         end
         Imax1(ii) = 1-thispeak;
 
-        [thispeak,~] = nigeLab.utils.peakseek(data(tlocmax(:,ii)),[],[],1);
+        [thispeak,~] = mypeakseek(data(tlocmax(:,ii)),[],[],1);
         if isempty(thispeak)
             thispeak = PLP;
         end
@@ -251,7 +265,7 @@ end
 
 
 
-
+%% FUNCTIONS
 function y = extendswt(x,lf)
 %EXTENDSWT extends the signal periodically at the boundaries
 [r,c] = size(x);
@@ -259,7 +273,6 @@ y = zeros(r+lf,c);
 y(1:lf/2,:) = x(end-lf/2+1:end,:);
 y(lf/2+1:lf/2+r,:) = x;
 y(end-lf/2+1:end,:) = x(1:lf/2,:);
-
 
 function X = myTEOcircshift(Y,k)
 %circshift without the boundary behaviour...
@@ -285,4 +298,116 @@ else
 end
 
 X = temp;
+
+function quant_X = mymovquant(X, p, n)
+%% MYMOVQUANT - calculate moving quantile
+%   Input parameters:
+%      X - 1-dimensional array
+%      p - Quantile to calculate. Must be between 0 and 1 (both included).
+%      n - Width of the moving window
+%
+% Written by Tommaso Lambresa 2024
+
+quant_X = zeros(size(X));
+
+% padding if signal length is not divisible for n
+remainder = mod(length(X), n);
+num_windows = floor(length(X) / n)+1;
+X_ax_pad = [X; zeros(n - remainder,1)];
+
+% Reshape the signal into a matrix where each column is a window
+X_matrix = reshape(X_ax_pad', n, num_windows);
+
+% Compute the quantile for each column
+quant = quantile(X_matrix, p, 1);
+
+% check if the p-th quantile is the maximum value, if so replace with the last
+% but one
+max_Xwin = max(X_matrix,[],1);
+critical_values = find(max_Xwin == quant);
+% Replace the last value with the last but one
+if ~isempty(critical_values)
+    data2sort = X_matrix(:,critical_values);
+    sorted_win = sort(data2sort);
+    quant(critical_values)=sorted_win(end-1,:);
+end
+
+
+quant_vector = repmat(quant, n, 1);  % Repeat each quantile value for each sample in the window
+quant_vector = quant_vector(:);  % Convert to a row vector
+
+quant_X = quant_vector(1:length(X));
+
+
+function [locs, pks]=mypeakseek(x,minpeakdist,minpeakh,npeaks)
+% Alternative to the findpeaks function.  This thing runs much much faster.
+%
+% x is a vector input (generally a timecourse)
+% minpeakdist is the minimum desired distance between peaks (optional, defaults to 1)
+% minpeakh is the minimum height of a peak (optional)
+% npeaks is the number of peak to detect in order of appearance
+%
+% modified by Tommaso Lambresa 2024, introduced the "npeaks" input
+
+switch nargin
+    case 1
+        minpeakdist = 1;
+        minpeakh = -inf;
+        npeaks = [];
+    case 2
+        minpeakh = -inf;
+        npeaks = [];
+    case 3
+        npeaks = [];
+    case 4
+        ...
+    otherwise
+        error("not ienough input arguments");
+end
+
+if isempty(minpeakdist)
+    minpeakdist = 1;
+end
+if isempty(minpeakh)
+    minpeakh = -inf;
+end
+
+
+if size(x,2)==1, x=x'; end
+
+% Find all maxima and ties
+locs=find(x(2:end-1)>=x(1:end-2) & x(2:end-1)>=x(3:end))+1;
+
+
+locs(x(locs)<=minpeakh)=[];
+
+if minpeakdist>1
+    while 1
+
+        del=diff(locs)<minpeakdist;
+
+        if ~any(del), break; end
+
+        pks=x(locs);
+
+        [garb, mins]=min([pks(del) ; pks([false del])]);
+
+        deln=find(del);
+
+        deln=[deln(mins==1) deln(mins==2)+1];
+
+        locs(deln)=[];
+
+    end
+end
+
+if ~isempty(npeaks) && ~isempty(locs)
+    locs = locs(1:npeaks);
+end
+
+if nargout>1
+    pks=x(locs);
+end
+
+
 
